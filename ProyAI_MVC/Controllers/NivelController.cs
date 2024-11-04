c42b02e6e8b4e627758d65f9a25695f8651d00b3
ricardo,zamorano@corfo.cl <143412619+rzamoranop@users.noreply.github.com>
2024-11-04 09:07:32 -0300

    Controlador de Nivel

---
+++ ProyAI_MVC/Controllers/NivelController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using ProyAI_MVC.Models;
+
+namespace ProyAI_MVC.Controllers
+{
+    public class NivelController : Controller
+    {
+
+        private readonly MiDbContext _context;
+        private readonly HttpClient _httpClient;
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
