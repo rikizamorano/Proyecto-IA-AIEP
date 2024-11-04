6be452248cb4c3955e389b1e693af3eec1a718c9
rikizamorano <ricardo.zamorano@corfo.cl>
2024-11-03 21:52:49 -0300

    Modelo unidad

---
+++ ProyAI_MVC/Models/Unidad.cs
@@ -0,0 +1,15 @@
+﻿using System.Security.Cryptography.Xml;
+
+namespace ProyAI_MVC.Models
+{
+    public class Unidad
+    {
+
+        public int Id_unidad { get; set; }
+        public string Nombre_unidad { get; set; } // Nombre de la unidad
+        public int Horas_pedagogicas { get; set; } // Relación con el modelo Asignatura
+        public int Asignatura_id { get; set; } // Navegación a Asignatura
+    }
+
+}
+}
