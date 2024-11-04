049516eaca1a070e55bf608cf16aaf103e4a2674
ricardo,zamorano@corfo.cl <143412619+rzamoranop@users.noreply.github.com>
2024-11-04 08:53:54 -0300

    Controlador de Nivel

---
+++ ProyAI_MVC/Controllers/NivelController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProyAI_MVC.Controllers
+{
+    public class NivelController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
